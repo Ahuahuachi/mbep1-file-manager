8b6d036308e857f8e617abcefb2687e0c2f870fa
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2023-07-12 18:32:27 -0600

    feat(files): add update function

--- files.py
+++ files.py
@@ -13,3 +13,18 @@ def create(file_name: str, content: str = None):
         file.write(content)
 
     file.close()
+
+
+def update(file_name: str, content: str, overwrite: bool = False):
+    """Updates an existing file
+
+    Args:
+        file_name (str): File name or path
+        content (str): Text file content
+        overwrite (bool, optional): If True, file will be overwritten. Defaults to False.
+    """
+    mode = "w" if overwrite else "a"
+
+    file = open(file_name, mode)
+    file.write(content)
+    file.close()
